a6da6af56ded952a18b0cf556abcc34fa5de6e74
RuchikaMishra45 <120638251+RuchikaMishra45@users.noreply.github.com>
2024-03-24 19:28:08 +0000

    conversion

---
+++ 01_basics/03_convert.js
@@ -0,0 +1,17 @@
+let score = undefined
+//console.log(typeof score);
+//console.log(typeof(score));
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let logged = 1
+let bool = Boolean(logged)
+console.log(bool);
+ // 1 => true; 0 => false
+ // "" => false
+ // "ruchika" => true
