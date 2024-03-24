6c73235ced1ab42f8be6f475f331f6277760e07d
RuchikaMishra45 <120638251+RuchikaMishra45@users.noreply.github.com>
2024-03-24 16:50:14 +0000

    container config

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,22 @@
+"use strict" // treat all JS code as newer verison
+// alert(3+3) //we are using nodejs , not browser
+console.log(3
+    +3) // code readability should be high
+console.log("Ruchika")
+
+//premitive datatypes
+
+// number => 2 to the power 53
+// bigint // use for big number
+// string => ""
+// boolean => true/false
+// null = standalone value // no value
+// undefined = value is not defined or assign
+// symbol = to find uniqueness // mostly use in react
+
+
+// object
+
+console.log(typeof "Ruchika");
+console.log(typeof null); // object
+console.log(typeof undefined);// undefined
